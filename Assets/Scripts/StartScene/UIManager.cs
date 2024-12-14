fa333c0168834591184115ac867cf99d8bba9b7e
HTL2910 <hathanhlong2910@gmail.com>
2024-12-14 09:11:31 +0700

    update code view count and  slider

--- Assets/Scripts/StartScene/UIManager.cs
+++ Assets/Scripts/StartScene/UIManager.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class UIManager : Singleton<UIManager>
 {
     [Header("Count")]
@@ -10,16 +11,25 @@ public class UIManager : Singleton<UIManager>
     public int maxStaminalCount;
     public float minspeed = 0f;
     public float maxSpeed = 0f;
-    public float currentSpeed = 0f;
+    public float currentSpeed = 0f;//
     public int priceStamina;
     public int priceSpeed;
     public int priceIncome;
     public int maxIncome=0;
+
+    private int staminaIncreasePrice = 2;
+    private int speedIncreasePrice = 3;
+    private int incomeIncreasePrice = 1; 
+    private int staminaIncreaseCount = 17;
+    private float speedIncreaseCount = 0.2f;
+    private int incomeIncreaseCount = 1;
+
+    private float speedSlider=0.5f;
     [Header("Text")]
     public TextMeshProUGUI moneyText;//amount
     public TextMeshProUGUI staminalText;
-    public TextMeshProUGUI minStaminalText;
-    public TextMeshProUGUI maxStaminalText;
+    public TextMeshProUGUI minSpeedText;
+    public TextMeshProUGUI maxSpeedText;
     public TextMeshProUGUI countStaminalText;
     public TextMeshProUGUI priceStaminalText;
     public TextMeshProUGUI countSpeedText;
@@ -29,21 +39,110 @@ public class UIManager : Singleton<UIManager>
 
     [Header("GameObject")]
     public GameObject settingPanel;
-    
-
-
-
-
-
+    [Header("Slider")]
+    public Slider staminaSlider;
+    public Slider soundSlider;
+    public Slider audioSlider;
+    public Slider vibrationSlider;
     void Start()
     {
-        
+        CheckSetting();
+        View();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void View()
     {
-        
+        moneyText.text = amount.ToString();
+        staminalText.text = currentStaminalCount.ToString() + "/" + maxStaminalCount.ToString();
+        minSpeedText.text = minspeed.ToString();
+        maxSpeedText.text = (maxSpeed * 10).ToString();
+        countStaminalText.text = maxStaminalCount.ToString();
+        priceStaminalText.text = priceStamina.ToString();
+        countSpeedText.text = maxSpeed.ToString();
+        priceSpeedText.text = priceSpeed.ToString();
+        countIncomeText.text = maxIncome.ToString();
+        priceIncomeText.text = priceIncome.ToString();
+        ViewSlider();
+    }
+    private void ViewSlider()
+    {
+        staminaSlider.value = currentStaminalCount / maxStaminalCount;
+
+    }
+
+    /// <summary>
+    /// update after
+    private int SliderValue(Slider slider)
+    {
+        return slider.value == 0 ? 1 : 0;
+    }
+    public void Sound()
+    {
+        float targetValue = (float)SliderValue(soundSlider);
+        StartCoroutine(SmoothSliderChange(soundSlider, targetValue, speedSlider));
+
+    }
+
+    public void Audio()
+    {
+        float targetValue = (float)SliderValue(audioSlider);
+        StartCoroutine(SmoothSliderChange(audioSlider, targetValue, speedSlider));
+    }
+    public void Vibration()
+    {
+        float targetValue = (float)SliderValue(vibrationSlider);
+        StartCoroutine(SmoothSliderChange(vibrationSlider, targetValue, speedSlider));
+    }
+    private IEnumerator SmoothSliderChange(Slider slider, float targetValue, float speed)
+    {
+        while (!Mathf.Approximately(slider.value, targetValue))
+        {
+            slider.value = Mathf.MoveTowards(slider.value, targetValue, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+    /// </summary>
+    private void CheckSetting()
+    {
+        if (settingPanel.activeSelf)
+        {
+            CloseSetting();
+        }
+    }
+
+
+    public void BuyStamina()
+    {
+        if (priceStamina < amount)
+        {
+            amount-=priceStamina;
+            priceStamina += staminaIncreasePrice;
+            maxStaminalCount += staminaIncreaseCount;
+        }
+        Debug.Log("Buy Stamina");
+        View();
+    }
+    public void BuySpeed()
+    {
+        if (priceSpeed < amount)
+        {
+            amount -= priceSpeed;
+            priceSpeed += speedIncreasePrice;
+            maxSpeed += speedIncreaseCount;
+        }
+        Debug.Log("Buy Speed");
+        View();
+    }
+    public void BuyIncome()
+    {
+        if (priceSpeed < amount)
+        {
+            amount -= priceIncome;
+            priceIncome += incomeIncreasePrice;
+            maxIncome += incomeIncreaseCount;
+        }
+        Debug.Log("Buy Income");
+        View();
     }
     public void Setting()
     {
