419459e67397d83d83e81901c215bb1aa31bd4f7
HTL2910 <hathanhlong2910@gmail.com>
2024-12-16 00:28:00 +0700

    question 2 required 1

---
+++ Assets/Scripts/GameScene/ChangeObjectNewMap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObjectNewMap : MonoBehaviour
+{
+    private float radius = 20f;
+
+    private void Start()
+    {
+        float x = Random.Range(-radius, radius);
+        float z = Random.Range(-radius, radius);
+        float rot = Random.Range(0f, 360f);
+        transform.SetPositionAndRotation(transform.position + new Vector3(x, 0f, z),
+            Quaternion.Euler(0f, rot, 0f));
+        Debug.Log(x+"+"+z);
+        Debug.Log("rot" + rot);
+    }
+}
